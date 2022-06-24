5800bd6942c31999f203e9ebd1670a3c12b66cef
MshSiam <siyamkenway@gmail.com>
2022-06-24 21:38:27 +0600

    added services

--- src/Pages/addservice/Addservice.js
+++ src/Pages/addservice/Addservice.js
@@ -1,9 +1,52 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+import axios from "axios"
+import "./addservice.css"
 
 const Addservice = () => {
+  const { register, handleSubmit, reset } = useForm()
+  const onSubmit = (data) => {
+    // post using axios
+    axios
+      .post("http://localhost:5000/services", data)
+      .then(function (response) {
+        console.log(response)
+        if (response.data.insertedId) {
+          alert("Added successfully.")
+          reset()
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    console.log(data)
+  }
+
   return (
     <div>
-      <h2>Add a Service</h2>
+      <h2 className="title">Add a Service</h2>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="name"
+          {...register("name", { required: true, maxLength: 20 })}
+        />
+        <textarea
+          type="text"
+          placeholder="description"
+          {...register("description", { required: true })}
+        />
+        <input
+          placeholder="price"
+          type="number"
+          {...register("price", { required: true })}
+        />
+        <input
+          placeholder="image-url"
+          type="text"
+          {...register("img", { required: true })}
+        />
+        <input type="submit" />
+      </form>
     </div>
   )
 }
